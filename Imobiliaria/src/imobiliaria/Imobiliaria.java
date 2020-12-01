d68cf1bc110f46863d74d1f5d14c4a422db2bfe7
thurzinSCRUM <ttbrm16@gmail.com>
2020-12-01 00:54:45 -0300

    First commit do thurzin

--- Imobiliaria/src/imobiliaria/Imobiliaria.java
+++ Imobiliaria/src/imobiliaria/Imobiliaria.java
@@ -16,6 +16,7 @@ public class Imobiliaria {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("alalal");
     }
     
 }
