ccda2c11532bf5ba127c76d139caa29bba89b53c
ewerton336 <65933715+ewerton336@users.noreply.github.com>
2020-12-01 21:36:26 -0300

    Add files via upload
    adicionado loop para selecionar a opção desejada.
    Criado opção de escolha (switch)

--- Imobiliaria/src/imobiliaria/Imobiliaria.java
+++ Imobiliaria/src/imobiliaria/Imobiliaria.java
@@ -5,6 +5,8 @@
  */
 package imobiliaria;
 
+import java.util.Scanner;
+
 /**
  * @author jg e thurzin
  */
@@ -14,7 +16,50 @@ public class Imobiliaria {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO 
+
+        Scanner teclado = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("***Imobiliária costamar *** \n Escolha uma opção:");
+            System.out.println("1) Cadastrar Imóvel \n 2) Alterar dados de imóvel \n 3) Pesquisar Imível \n 4) Excluir Imóvel \n 5) Relatórios \n 6) Sair");
+            opcao = teclado.nextInt();
+            teclado.close();
+        } while (opcao > 6);
+
+
+        switch (opcao){
+
+           case 1:
+           System.out.println("Opção 1");
+           break;
+
+
+           case 2:
+           System.out.println("Opção 2");
+           break;
+
+           case 3:
+           System.out.println("Opção 3");
+           break;
+
+           case 4:
+           System.out.println("Opção 4");
+           break;
+
+           case 5:
+           System.out.println("Opção 5");
+           break;
+
+           case 6:
+           break;
+
+        }
+
+
+
+
+        
     }
     
 }
